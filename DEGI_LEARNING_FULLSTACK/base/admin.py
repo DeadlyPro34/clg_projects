3263650dc212126e81af49c3201f06191e643b41
Akhil Biju Varghese <akhil.sbj@gmail.com>
2026-04-04 09:19:19 +0530

    Removed
    This file have been removed

--- DEGI_LEARNING_FULLSTACK/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Course, Purchase, ContactMessage, CourseCompletion
-
-admin.site.register(Profile)
-admin.site.register(Course)
-admin.site.register(Purchase)
-admin.site.register(ContactMessage)
-admin.site.register(CourseCompletion)
